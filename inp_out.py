a2e87b251b750f11c7570211dd39e801d600f826
scuer90 <scuer90@gmail.com>
2020-04-03 00:15:33 +0500

    Redact New_redact

---
+++ inp_out.py
@@ -0,0 +1,13 @@
+# comments
+
+'''
+
+one line
+two line
+matherfucker comments
+
+'''
+
+print('space defender')
+print('space', 'defender', 3000)
+print('space', 'defender', 3000, sep='sss')
---
+++ sex_girl.py
@@ -0,0 +1,11 @@
+# Имя
+name = 'Valensiya'
+print(name, type(name))
+
+# Возраст
+age = 18
+print(age, type(age))
+
+# Размер груди
+tits = 13.3
+print(tits, type(tits))
